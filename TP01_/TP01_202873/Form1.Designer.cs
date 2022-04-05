3b047f96943e7a0bab323a1d97c3aea0bc7570b6
MuriloBraz <murilobraz44@gmail.com>
2022-04-04 21:55:49 -0300

    ArquivosProjeto

---
+++ TP01_/TP01_202873/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace TP01_202873
+{
+    partial class FrmInicial
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PnlDelta = new System.Windows.Forms.Panel();
+            this.TxtR3 = new System.Windows.Forms.TextBox();
+            this.TxtR2 = new System.Windows.Forms.TextBox();
+            this.TxtR1 = new System.Windows.Forms.TextBox();
+            this.LblR3 = new System.Windows.Forms.Label();
+            this.LblR2 = new System.Windows.Forms.Label();
+            this.LblR1 = new System.Windows.Forms.Label();
+            this.LblDelta = new System.Windows.Forms.Label();
+            this.PnlY = new System.Windows.Forms.Panel();
+            this.TxtYR6 = new System.Windows.Forms.TextBox();
+            this.TxtYR5 = new System.Windows.Forms.TextBox();
+            this.TxtYR4 = new System.Windows.Forms.TextBox();
+            this.LblYR6 = new System.Windows.Forms.Label();
+            this.LblYR5 = new System.Windows.Forms.Label();
+            this.LblYR4 = new System.Windows.Forms.Label();
+            this.LblY = new System.Windows.Forms.Label();
+            this.PnlBaixo = new System.Windows.Forms.Panel();
+            this.BtnLimpar = new System.Windows.Forms.Button();
+            this.BtnCalc = new System.Windows.Forms.Button();
+            this.BtnSair = new System.Windows.Forms.Button();
+            this.LblControl = new System.Windows.Forms.Label();
+            this.PnlDelta.SuspendLayout();
+            this.PnlY.SuspendLayout();
+            this.PnlBaixo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PnlDelta
+            // 
+            this.PnlDelta.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.PnlDelta.Controls.Add(this.TxtR3);
+            this.PnlDelta.Controls.Add(this.TxtR2);
+            this.PnlDelta.Controls.Add(this.TxtR1);
+            this.PnlDelta.Controls.Add(this.LblR3);
+            this.PnlDelta.Controls.Add(this.LblR2);
+            this.PnlDelta.Controls.Add(this.LblR1);
+            this.PnlDelta.Controls.Add(this.LblDelta);
+            this.PnlDelta.Location = new System.Drawing.Point(12, 12);
+            this.PnlDelta.Name = "PnlDelta";
+            this.PnlDelta.Size = new System.Drawing.Size(200, 264);
+            this.PnlDelta.TabIndex = 0;
+            // 
+            // TxtR3
+            // 
+            this.TxtR3.Location = new System.Drawing.Point(76, 179);
+            this.TxtR3.Name = "TxtR3";
+            this.TxtR3.Size = new System.Drawing.Size(100, 20);
+            this.TxtR3.TabIndex = 7;
+            // 
+            // TxtR2
+            // 
+            this.TxtR2.Location = new System.Drawing.Point(76, 122);
+            this.TxtR2.Name = "TxtR2";
+            this.TxtR2.Size = new System.Drawing.Size(100, 20);
+            this.TxtR2.TabIndex = 6;
+            // 
+            // TxtR1
+            // 
+            this.TxtR1.Location = new System.Drawing.Point(76, 71);
+            this.TxtR1.Name = "TxtR1";
+            this.TxtR1.Size = new System.Drawing.Size(100, 20);
+            this.TxtR1.TabIndex = 1;
+            // 
+            // LblR3
+            // 
+            this.LblR3.AutoSize = true;
+            this.LblR3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblR3.Location = new System.Drawing.Point(19, 179);
+            this.LblR3.Name = "LblR3";
+            this.LblR3.Size = new System.Drawing.Size(33, 24);
+            this.LblR3.TabIndex = 5;
+            this.LblR3.Text = "R3";
+            // 
+            // LblR2
+            // 
+            this.LblR2.AutoSize = true;
+            this.LblR2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblR2.Location = new System.Drawing.Point(19, 122);
+            this.LblR2.Name = "LblR2";
+            this.LblR2.Size = new System.Drawing.Size(33, 24);
+            this.LblR2.TabIndex = 4;
+            this.LblR2.Text = "R2";
+            // 
+            // LblR1
+            // 
+            this.LblR1.AutoSize = true;
+            this.LblR1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblR1.Location = new System.Drawing.Point(19, 71);
+            this.LblR1.Name = "LblR1";
+            this.LblR1.Size = new System.Drawing.Size(33, 24);
+            this.LblR1.TabIndex = 1;
+            this.LblR1.Text = "R1";
+            // 
+            // LblDelta
+            // 
+            this.LblDelta.AutoSize = true;
+            this.LblDelta.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblDelta.Location = new System.Drawing.Point(18, 0);
+            this.LblDelta.Name = "LblDelta";
+            this.LblDelta.Size = new System.Drawing.Size(159, 29);
+            this.LblDelta.TabIndex = 3;
+            this.LblDelta.Text = "Cicuito Triangulo";
+            // 
+            // PnlY
+            // 
+            this.PnlY.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.PnlY.Controls.Add(this.TxtYR6);
+            this.PnlY.Controls.Add(this.TxtYR5);
+            this.PnlY.Controls.Add(this.TxtYR4);
+            this.PnlY.Controls.Add(this.LblYR6);
+            this.PnlY.Controls.Add(this.LblYR5);
+            this.PnlY.Controls.Add(this.LblYR4);
+            this.PnlY.Controls.Add(this.LblY);
+            this.PnlY.Location = new System.Drawing.Point(235, 12);
+            this.PnlY.Name = "PnlY";
+            this.PnlY.Size = new System.Drawing.Size(200, 264);
+            this.PnlY.TabIndex = 8;
+            // 
+            // TxtYR6
+            // 
+            this.TxtYR6.Location = new System.Drawing.Point(76, 179);
+            this.TxtYR6.Name = "TxtYR6";
+            this.TxtYR6.Size = new System.Drawing.Size(100, 20);
+            this.TxtYR6.TabIndex = 7;
+            // 
+            // TxtYR5
+            // 
+            this.TxtYR5.Location = new System.Drawing.Point(76, 122);
+            this.TxtYR5.Name = "TxtYR5";
+            this.TxtYR5.Size = new System.Drawing.Size(100, 20);
+            this.TxtYR5.TabIndex = 6;
+            // 
+            // TxtYR4
+            // 
+            this.TxtYR4.Location = new System.Drawing.Point(76, 71);
+            this.TxtYR4.Name = "TxtYR4";
+            this.TxtYR4.Size = new System.Drawing.Size(100, 20);
+            this.TxtYR4.TabIndex = 1;
+            // 
+            // LblYR6
+            // 
+            this.LblYR6.AutoSize = true;
+            this.LblYR6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblYR6.Location = new System.Drawing.Point(19, 179);
+            this.LblYR6.Name = "LblYR6";
+            this.LblYR6.Size = new System.Drawing.Size(33, 24);
+            this.LblYR6.TabIndex = 5;
+            this.LblYR6.Text = "R6";
+            // 
+            // LblYR5
+            // 
+            this.LblYR5.AutoSize = true;
+            this.LblYR5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblYR5.Location = new System.Drawing.Point(19, 122);
+            this.LblYR5.Name = "LblYR5";
+            this.LblYR5.Size = new System.Drawing.Size(33, 24);
+            this.LblYR5.TabIndex = 4;
+            this.LblYR5.Text = "R5";
+            // 
+            // LblYR4
+            // 
+            this.LblYR4.AutoSize = true;
+            this.LblYR4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblYR4.Location = new System.Drawing.Point(19, 71);
+            this.LblYR4.Name = "LblYR4";
+            this.LblYR4.Size = new System.Drawing.Size(33, 24);
+            this.LblYR4.TabIndex = 1;
+            this.LblYR4.Text = "R4";
+            // 
+            // LblY
+            // 
+            this.LblY.AutoSize = true;
+            this.LblY.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblY.Location = new System.Drawing.Point(38, 0);
+            this.LblY.Name = "LblY";
+            this.LblY.Size = new System.Drawing.Size(138, 29);
+            this.LblY.TabIndex = 3;
+            this.LblY.Text = "Cicuito Estrela";
+            // 
+            // PnlBaixo
+            // 
+            this.PnlBaixo.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.PnlBaixo.Controls.Add(this.BtnLimpar);
+            this.PnlBaixo.Controls.Add(this.BtnCalc);
+            this.PnlBaixo.Controls.Add(this.BtnSair);
+            this.PnlBaixo.Location = new System.Drawing.Point(12, 294);
+            this.PnlBaixo.Name = "PnlBaixo";
+            this.PnlBaixo.Size = new System.Drawing.Size(423, 88);
+            this.PnlBaixo.TabIndex = 9;
+            // 
+            // BtnLimpar
+            // 
+            this.BtnLimpar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnLimpar.Location = new System.Drawing.Point(153, 25);
+            this.BtnLimpar.Name = "BtnLimpar";
+            this.BtnLimpar.Size = new System.Drawing.Size(113, 34);
+            this.BtnLimpar.TabIndex = 4;
+            this.BtnLimpar.Text = "Limpar";
+            this.BtnLimpar.UseVisualStyleBackColor = true;
+            this.BtnLimpar.Click += new System.EventHandler(this.BtnLimpar_Click_1);
+            // 
+            // BtnCalc
+            // 
+            this.BtnCalc.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnCalc.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCalc.Location = new System.Drawing.Point(23, 25);
+            this.BtnCalc.Name = "BtnCalc";
+            this.BtnCalc.Size = new System.Drawing.Size(113, 34);
+            this.BtnCalc.TabIndex = 3;
+            this.BtnCalc.Text = "Calcular";
+            this.BtnCalc.UseVisualStyleBackColor = true;
+            this.BtnCalc.Click += new System.EventHandler(this.BtnCalc_Click_1);
+            // 
+            // BtnSair
+            // 
+            this.BtnSair.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnSair.Location = new System.Drawing.Point(286, 25);
+            this.BtnSair.Name = "BtnSair";
+            this.BtnSair.Size = new System.Drawing.Size(113, 34);
+            this.BtnSair.TabIndex = 2;
+            this.BtnSair.Text = "Sair";
+            this.BtnSair.UseVisualStyleBackColor = true;
+            this.BtnSair.Click += new System.EventHandler(this.BtnSair_Click);
+            // 
+            // LblControl
+            // 
+            this.LblControl.AutoSize = true;
+            this.LblControl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblControl.Location = new System.Drawing.Point(12, 279);
+            this.LblControl.Name = "LblControl";
+            this.LblControl.Size = new System.Drawing.Size(101, 24);
+            this.LblControl.TabIndex = 11;
+            this.LblControl.Text = ".:Controle:.";
+            this.LblControl.Click += new System.EventHandler(this.LblControl_Click);
+            // 
+            // FrmInicial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(450, 396);
+            this.Controls.Add(this.LblControl);
+            this.Controls.Add(this.PnlBaixo);
+            this.Controls.Add(this.PnlY);
+            this.Controls.Add(this.PnlDelta);
+            this.Name = "FrmInicial";
+            this.Text = "Conversão de Circuitos";
+            this.PnlDelta.ResumeLayout(false);
+            this.PnlDelta.PerformLayout();
+            this.PnlY.ResumeLayout(false);
+            this.PnlY.PerformLayout();
+            this.PnlBaixo.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PnlDelta;
+        private System.Windows.Forms.TextBox TxtR3;
+        private System.Windows.Forms.TextBox TxtR2;
+        private System.Windows.Forms.TextBox TxtR1;
+        private System.Windows.Forms.Label LblR3;
+        private System.Windows.Forms.Label LblR2;
+        private System.Windows.Forms.Label LblR1;
+        private System.Windows.Forms.Label LblDelta;
+        private System.Windows.Forms.Panel PnlY;
+        private System.Windows.Forms.TextBox TxtYR6;
+        private System.Windows.Forms.TextBox TxtYR5;
+        private System.Windows.Forms.TextBox TxtYR4;
+        private System.Windows.Forms.Label LblYR6;
+        private System.Windows.Forms.Label LblYR5;
+        private System.Windows.Forms.Label LblYR4;
+        private System.Windows.Forms.Label LblY;
+        private System.Windows.Forms.Panel PnlBaixo;
+        private System.Windows.Forms.Label LblControl;
+        private System.Windows.Forms.Button BtnSair;
+        private System.Windows.Forms.Button BtnCalc;
+        private System.Windows.Forms.Button BtnLimpar;
+    }
+}
+
---
+++ TP01_/TP01_202873/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP01_202873
+{
+    public partial class FrmInicial : Form
+    {
+        public FrmInicial()
+        {
+            InitializeComponent();
+        }
+
+        private void LblDelta_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LblControl_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void BtnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void BtnCalc_Click_1(object sender, EventArgs e)
+        {
+            double R1;
+            double R2;
+            double R3;
+
+            double R4;
+            double R5;
+            double R6;
+
+            R1 = double.Parse(TxtR1.Text);
+            R2 = double.Parse(TxtR2.Text);
+            R3 = double.Parse(TxtR3.Text);
+
+            R4 = R1 * R2 / (R1 + R2 + R3);
+            R5 = R2 * R3 / (R1 + R2 + R3);
+            R6 = R3 * R1 / (R1 + R2 + R3);
+
+            TxtYR4.Text = R4.ToString();
+            TxtYR5.Text = R5.ToString();
+            TxtYR6.Text = R6.ToString();
+        }
+
+        private void BtnLimpar_Click_1(object sender, EventArgs e)
+        {
+            TxtR1.Text = "";
+            TxtR2.Text = "";
+            TxtR3.Text = "";
+            TxtYR4.Text = "";
+            TxtYR5.Text = "";
+            TxtYR6.Text = "";
+        }
+    }
+}
